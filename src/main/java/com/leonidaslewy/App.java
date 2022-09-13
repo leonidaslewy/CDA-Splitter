b6d414ffee907fd81ff5a085aa8bb08af0626465
Leônidas Lewy <leonidas.lewy@gmail.com>
2022-09-13 19:20:50 -0300

    pom dependecies adjusted

--- src/main/java/com/leonidaslewy/App.java
+++ src/main/java/com/leonidaslewy/App.java
@@ -1,9 +1,7 @@
 package com.leonidaslewy;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
